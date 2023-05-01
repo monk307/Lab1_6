f02412516aba31ec980d64538baeb2e50bf18066
Solominov <mateuspost@gmail.com>
2023-05-01 19:34:57 +0300

    Lab1_6

--- lab_3/blog/blog/urls.py
+++ lab_3/blog/blog/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from articles import views
 
 urlpatterns = [
+    re_path(r'^article/(?P<article_id>\d+)$', views.get_article, name='get_article'),
     path('admin/', admin.site.urls),
     path('', views.archive, name='home'),
 ]
+
